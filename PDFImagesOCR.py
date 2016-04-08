52aa68e6960ed5dd9f82c680d69ead88ce780396
alset333 <alset333@icloud.com>
2016-04-08 13:44:35 -0400

    Added (experimental) support for outputting to searchable PDF. Also re-wrote some methods into a 'Core' file.

--- PDFImagesOCR.py
+++ PDFImagesOCR.py
@@ -1,100 +1,89 @@
 #!/usr/bin/env python3
 # PDFImagesOCR.py
 
-import DependencyHandler
+import PDFImagesOCRCore
+import PyPDF2
 import sys
 import os
 import tempfile
 import shutil
 
 __author__ = "Peter Maar"
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 
-def pdfToImages(ifp, tp):
-    """Takes a PDF from the input-file-path (ifp), and converts it into images as 'pg-*.jpg' in the temporary path (tp). Returns the number of pages."""
-    print("Converting PDF into images...")
-    os.system('convert -density 300 "' + ifp + '" "' + os.path.normpath(tp + '/pg.jpg') + '"')
-    files = os.listdir(tp)
-    if '.DS_Store' in files:
-        files.remove('.DS_Store')
-    pgCount = len(files) # The number of pages is the number of files
-    print("PDF Converted.")
-    return pgCount
+
+############# METHOD DEFINITIONS START #################
 
 def imagesToTxt(pgCount, tp, ofp):
     """Takes the temporary path (tp) where pgCount images are stored, and OCRs them into a txt file (ofp)"""
+
+    # OCR the images
     for i in range(pgCount): # Starting at 0, and up to (but not including) pgCount - this works since counting pages starts at 1, but the files start at 0.
         imagePath = os.path.normpath(tp + "/pg-" + str(i) + ".jpg")
         print("Reading page", i + 1, "of", str(pgCount) + "...")
         if DEBUGMODE:
-            print('tesseract "' + imagePath + '" stdout >> "' + ofp + '"')
-        os.system('tesseract "' + imagePath + '" stdout >> "' + ofp + '"')
+            print('tesseract "' + imagePath + '" "' + ofp + str(i) + '"')
+        os.system('tesseract "' + imagePath + '" "' + ofp + str(i) + '"')
+
+    # Combine the output files
+    outfile = open(ofp, 'a')
+    for i in range(pgCount):
+        infile = open(ofp + str(i) + '.txt')
+        outfile.write(infile.read())
+        infile.close()
+        os.remove(ofp + str(i) + '.txt') # Delete the file after it's been appended
+    outfile.close()
+
     print("All pages read.")
+
+def imagesToPDF(pgCount, tp, ofp):
+    """Takes the temporary path (tp) where pgCount images are stored, and OCRs them into a pdf file (ofp)"""
+    # OCR the images
+    for i in range(pgCount):  # Starting at 0, and up to (but not including) pgCount - this works since counting pages starts at 1, but the files start at 0.
+        imagePath = os.path.normpath(tp + "/pg-" + str(i) + ".jpg")
+        print("Reading page", i + 1, "of", str(pgCount) + "...")
+        if DEBUGMODE:
+            print('tesseract "' + imagePath + '" "' + ofp + str(i) + '" pdf')
+        os.system('tesseract "' + imagePath + '" "' + ofp + str(i) + '" pdf')
+
+    # Combine the output files
+    merger = PyPDF2.PdfFileMerger()  # New PDF
+    for i in range(pgCount):
+        merger.append(ofp + str(i) + '.pdf')
+        os.remove(ofp + str(i) + '.pdf')  # Delete the file after it's been appended
+    merger.write(ofp)
+
+    print("All pages read.")
+
+########### METHOD DEFINITIONS END ######################
+
+
+
+
+
+
+
 
 
 ##################################  PREPARATIONS BEGIN  ###############################################################
 
+
 # Print help text if needed
-if (len(sys.argv) != 3 and len(sys.argv) != 4) or 'help' in sys.argv or '-h' in sys.argv or '/?' in sys.argv or '--help' in sys.argv:
-    print("""
-
-NAME
-    PDFImagesOCR - PDF to txt or searchable PDF
-
-COMMAND LINE SYNTAX
-    PDFImagesOCR inFile outType
-
-PARTS/OPTIONS
-    PDFImagesOCR
-        Command to execute the 'PDFImagesOCR.py' file.
-    inFile
-        The path to the PDF input.
-    outType
-        Either 'txt' or 'pdf'. Specifies whether to output to a txt file,
-         or to a searchable PDF file.
-
-EXAMPLE
-    In a folder with 'PDFImagesOCR.py' and 'file.pdf', output to 'file-OCR.txt':
-        Unix (Mac/Linux):
-            "./PDFImagesOCR.py file.pdf txt"
-        Windows:
-            (untested so far)
-
-DESCRIPTION
-    Converts images from a PDF into a text file or a searchable PDF.
-    PDFImagesOCR requires 'ImageMagick', and the OCR Engine 'tesseract' to both be installed.
-    The file output will be saved alongside the original file,
-     as the original's name with '-OCR' appended to it.
-
-
-
-REQUIREMENTS
-    Python 3
-        PDFImagesOCR is written to be run by Python 3.
-    ImageMagick (CLI)
-        ImageMagick is needed to convert the PDF into JPEGs.
-        ImageMagick's convert command must be accessible from the command line as 'convert'.
-    Tesseract OCR Engine (CLI)
-        The Tesseract OCR Engine is needed for reading text from the images.
-        It must be accessible from the command line as 'tesseract'.
-
-""")
+if PDFImagesOCRCore.shouldPrintHelp(sys.argv):
+    print(PDFImagesOCRCore.HELPTEXT)
     exit()
 
 # Check if the program should run in debug mode
 DEBUGMODE = 'DEBUG' in sys.argv
 
 # Check dependencies
-print("Checking dependencies...")
-if DependencyHandler.dependencies():
-    print("Dependencies seem good! Proceeding.")
-else:
-    print("Dependencies not detected or not working,\nand could not be installed automatically.\nPlease install and configure them.")
-    exit()
+PDFImagesOCRCore.checkDepend()
 
 # Get Input File Location
 inFilePath = os.path.normpath(sys.argv[1])
+
+# Check the input file exists
 if not os.path.isfile(inFilePath):
     print("Input file doesn't exist!")
     exit()
@@ -102,18 +91,24 @@ if not os.path.isfile(inFilePath):
 # Get Output File Location
 outType = sys.argv[2].lower()
 outFilePath = inFilePath[:-4] + "-OCR." + outType
+
+if outType == 'pdf':
+    ynOut = input('Searchable PDF output is still highly experimental. Proceed? (y/n)')
+    if ynOut != 'y':
+        print("Response did not match 'y', exiting.")
+        exit()
+
+# Check that the output file doesn't exist
 if os.path.isfile(outFilePath):
     print("Output file already exists!")
     exit()
 
-# Get Temporary Location
+# Find & Make Temporary Location
 tempPath = os.path.normpath(tempfile.gettempdir() + "/PDFImagesOCR-TempFolder")
 os.mkdir(tempPath)
 
 if DEBUGMODE:
     print('In: ' + inFilePath + '\n' + 'Out: ' + outFilePath + '\n' + 'Temp: ' + tempPath + '\n\n')
-
-
 
 
 ###################################  PREPARATIONS END  ################################################################
@@ -123,12 +118,16 @@ if DEBUGMODE:
 
 
 
+
+
 # Use ImageMagick to turn the pages into images
-pageCount = pdfToImages(inFilePath, tempPath) # Pagecount is the number of pages, not the number of the last image
+pageCount = PDFImagesOCRCore.pdfToImages(inFilePath, tempPath) # Pagecount is the number of pages, not the number of the last image
 
 
 if outType == 'txt':
     imagesToTxt(pageCount, tempPath, outFilePath)
+elif outType == 'pdf':
+    imagesToPDF(pageCount, tempPath, outFilePath)
 else:
     print("Unknown output type!")
 
---
+++ PDFImagesOCRCore.py
@@ -0,0 +1,68 @@
+import DependencyHandler
+import os
+
+HELPTEXT = """
+
+NAME
+    PDFImagesOCR - PDF to txt or searchable PDF
+
+COMMAND LINE SYNTAX
+    PDFImagesOCR inFile outType
+
+PARTS/OPTIONS
+    PDFImagesOCR
+        Command to execute the 'PDFImagesOCR.py' file.
+    inFile
+        The path to the PDF input.
+    outType
+        Either 'txt' or 'pdf'. Specifies whether to output to a txt file,
+         or to a searchable PDF file.
+
+EXAMPLE
+    In a folder with 'PDFImagesOCR.py' and 'file.pdf', output to 'file-OCR.txt':
+        Unix (Mac/Linux):
+            "./PDFImagesOCR.py file.pdf txt"
+        Windows:
+            (untested so far)
+
+DESCRIPTION
+    Converts images from a PDF into a text file or a searchable PDF.
+    PDFImagesOCR requires 'ImageMagick', and the OCR Engine 'tesseract' to both be installed.
+    The file output will be saved alongside the original file,
+     as the original's name with '-OCR' appended to it.
+
+
+
+REQUIREMENTS
+    Python 3
+        PDFImagesOCR is written to be run by Python 3.
+    ImageMagick (CLI)
+        ImageMagick is needed to convert the PDF into JPEGs.
+        ImageMagick's convert command must be accessible from the command line as 'convert'.
+    Tesseract OCR Engine (CLI)
+        The Tesseract OCR Engine is needed for reading text from the images.
+        It must be accessible from the command line as 'tesseract'.
+
+"""
+
+def shouldPrintHelp(argv):
+    return (len(argv) != 3 and len(argv) != 4) or 'help' in argv or '-h' in argv or '/?' in argv or '--help' in argv
+
+def checkDepend():
+    print("Checking dependencies...")
+    if DependencyHandler.dependencies():
+        print("Dependencies seem good! Proceeding.")
+    else:
+        print("Dependencies not detected or not working,\nand could not be installed automatically.\nPlease install and configure them.")
+        exit()
+
+def pdfToImages(ifp, tp):
+    """Takes a PDF from the input-file-path (ifp), and converts it into images as 'pg-*.jpg' in the temporary path (tp). Returns the number of pages."""
+    print("Converting PDF into images...")
+    os.system('convert -density 300 "' + ifp + '" "' + os.path.normpath(tp + '/pg.jpg') + '"')
+    files = os.listdir(tp)
+    if '.DS_Store' in files:
+        files.remove('.DS_Store')
+    pgCount = len(files) # The number of pages is the number of files
+    print("PDF Converted.")
+    return pgCount
