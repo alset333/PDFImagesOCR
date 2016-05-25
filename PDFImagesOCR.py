67b26d54406d6f37cd0b4f961a417f7fff54839b
alset333 <alset333@icloud.com>
2016-05-25 12:36:41 -0400

    Only import needed UI

--- PDFImagesOCR.py
+++ PDFImagesOCR.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 import sys
-import gui
-import cli
 
 __author__ = 'Peter Maar'
 __version__ = '0.7.0'  # Added CLI. There is now a GUI _and_ a CLI.
 
 if len(sys.argv) == 1:  # If the only argument is the file, run the GUI
+    import gui  # Only import if needed, especially important if they're headless with no tkinter
     g = gui.Gui()
 else:                   # If there are other arguments, run the CLI with them
+    import cli  # Only import if needed,
     c = cli.Cli(sys.argv)
